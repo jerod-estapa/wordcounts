0b8f845710d7466f8c66b84e95bb3d0e7ce0b84e
jerodestapa <jerodestapa@gmail.com>
2015-08-06 23:51:43 -0500

    Added models.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/ python3
+import os
 
 
 class Config(object):
@@ -7,7 +8,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
 
@@ -29,3 +30,4 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
 
     TESTING = True
+
