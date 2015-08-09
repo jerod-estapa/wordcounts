bda54dcba922441b1b222c14027f478fba8ef3d5
jerodestapa <jerodestapa@gmail.com>
2015-08-08 20:20:26 -0500

    Added index.html

--- app.py
+++ app.py
@@ -7,17 +7,11 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import Result
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
-@app.route('/')
-def hello():
-    return 'Hello, World'
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello, {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
